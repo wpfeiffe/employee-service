129a83f9fb5d490b55fa9e60f4750bf603bfb414
Bill Pfeiffer <bill.pfeiffer@gmail.com>
2018-12-12 14:09:24 -0500

    Fixed dto to use Lombok

--- src/main/java/com/wspfeiffer/micro/employeeservice/dto/EmployeeDTO.java
+++ src/main/java/com/wspfeiffer/micro/employeeservice/dto/EmployeeDTO.java
@@ -1,7 +1,9 @@
 package com.wspfeiffer.micro.employeeservice.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
 
+@Data
 public class EmployeeDTO {
     @JsonProperty("id")
     Long id;
@@ -17,47 +19,6 @@ public class EmployeeDTO {
 
     @JsonProperty("title")
     String title;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(final Long id) {
-        this.id = id;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(final String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(final String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(final String email) {
-        this.email = email;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(final String title) {
-        this.title = title;
-    }
-
-
+    
 }
 
